10afdad24b6681b91a310e28611d55b6a1343b80
KevinDazaR <61398857+KevinDazaR@users.noreply.github.com>
2024-01-05 15:58:51 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-
-const LISTA_USUARIO = [
-    { name: "Jose", email: "joseLuis@gmail.com", password: "jose123" },
-    { name: "Manuel", email: "manuelAlejandro@gmail.com", password: "mauel123" },
-    { name: "Luis", email: "luisAlvarez@gmail.com", password: "luis123" }
+let LISTA_USUARIO = [
+    { name: "Jose", email: "joseLuis@gmail.com", password: "jose123", gender:'male' },
+    { name: "Manuela", email: "manuelaAlejandra@gmail.com", password: "mauela123", gender:'male' },
+    { name: "Luis", email: "luisAlvarez@gmail.com", password: "luis123", gender:'male'}
 ];
 
-let usuario = {
-    name: '',
-    email: '',
-    password: ''
-}
 
 let userNameValue = document.getElementById("nameText");
 let userEmailValue = document.getElementById("emailText");
 let userPasswordValue = document.getElementById("passwordText");
+
+let UsergenderSelect = document.getElementById("genderSelect");
+let femaleGender = document.getElementById("female");
+let maleGender = document.getElementById("female");
+let otherGender = document.getElementById("female");
+
 let invalidData1 = document.getElementById("invalid-feedback1"); /* name div de invalid */
 let invalidData2 = document.getElementById("invalid-feedback2"); /* name div de invalid */
 let invalidData3 = document.getElementById("invalid-feedback3"); /* name div de invalid */
@@ -23,12 +23,24 @@ let validarEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 let btnSingUp = document.getElementById('button-singUp');
 
 var datoInvalidoFunction;
-
+let formBoxLogin = document.getElementById("login-form-box");
 let contadorValidoSingUp = 0;
+var loginStyleCounter = 0;
 
 /* REGISTER SECTION */
+
+
+
+
+
 btnSingUp.addEventListener("click",function singUp(){
 
+    let usuario = {
+        name: '',
+        email: '',
+        password: '',
+        gender:''
+    }
 
     /* si es invàlido o vacìo el dato */
     
@@ -110,6 +122,8 @@ btnSingUp.addEventListener("click",function singUp(){
             userNameValue.value = "";
             userEmailValue.value = "";
             userPasswordValue.value = "";
+            UsergenderSelect.value = "default";
+            // userPasswordValue.value = ""; /* FALTA QUITAR EL VALUE DEL GENERO */
             userNameValue.classList.remove("valid");
             userNameValue.classList.remove("invalid");
             userEmailValue.classList.remove("valid");
@@ -119,6 +133,8 @@ btnSingUp.addEventListener("click",function singUp(){
             invalidData1.style.display = "none";
             invalidData2.style.display = "none";
             invalidData3.style.display = "none";
+            console.log(LISTA_USUARIO);
+
         }
         
 
@@ -128,34 +144,36 @@ btnSingUp.addEventListener("click",function singUp(){
 
     function enterLogin(){
         alert("Registro Exitoso!")
-        let formBoxLogin = document.getElementById("login-form-box");
-        formBoxLogin.style.top = "32%";
-    
     }
 
+    console.log(usuario);
     /* validar si el input no es vacìo, enviar formulario */
 
+
     if((userNameValue.value !=="") && (userEmailValue.value !=="") && (userPasswordValue.value !=="")){
+        console.log("nuevo registro")
         usuario.name = userNameValue.value;
         usuario.email = userEmailValue.value;
         usuario.password = userPasswordValue.value;
-        console.log(usuario);
-         borrarDatosInput();
-        
+        usuario.gender = UsergenderSelect.value;
+        LISTA_USUARIO.push(usuario);
+        console.log(usuario, LISTA_USUARIO);
+        borrarDatosInput();
     }
 
     else{
         datoInvalido();
     }
     
-    LISTA_USUARIO.push(usuario);
-    
-    console.log()
-    
+    sessionStorage.setItem('listaUsuarios',JSON.stringify(LISTA_USUARIO, 2));
+    console.log(sessionStorage)
 
 })
 
+
+
 /*---------- LOGIN -----------*/
+
 let btnLogin = document.getElementById("button-login");
 let loginName = document.getElementById("nameTextLogin")
 let loginPassword = document.getElementById("nameTextSingUp");
@@ -167,7 +185,10 @@ btnLogin.addEventListener("click",function login(event){
 
     LISTA_USUARIO.forEach(function(person){
         if(person.email === loginName.value && person.password === loginPassword.value){
+            sessionStorage.setItem('usuario',usuario);  /* ERROR AL VALIDAR USUARIO POR SCOPE , CREADO DENTRO DE UNA FUCION */
             console.log("Inicio exitoso");
+            location.href ="../html/userLogged.html";
+            
         }
         else if(person.email != loginName.value || person.password != loginPassword.value){
             console.log("Usuario o contraseña invàlido, intente de nuevo")
@@ -176,154 +197,28 @@ btnLogin.addEventListener("click",function login(event){
             console.log("Usuario invàlido")
         }
     });
-    /* COMPARAR OBJETO NAME CON LOGIN NAME */
+    // COMPARAR OBJETO NAME CON LOGIN NAME
+
+
+
+
 
 }) 
 
 
-/* PRUEBA PONER Y QUITAR CLASE QUE INFORME*/
-
-// let usuarios = [];
-
-
-// function singUp(){
-
-//     let userNameValue = document.getElementBy("nameText").value;
-
-//     let userEmailValue =document.getElementById("emailText").value;
-    
-//     let userPasswordValue =document.getElementById("passwordText");
-    
-//     console.log(userPasswordValue.value)
-
-//     
-
-//     usuarios.push(usuario);
-    
-//     console.log(usuario.name)
-
-// }
-
-// let usuarios = [];
-
-// let usuario = {
-//     name: '',
-//     email: '',
-//     password: ''
-// }
-
-// let btnSingUp = document.getElementById('button-singUp');
-
-
-// btnSingUp.addEventListener("click",function singUp(){
-
-//     let userNameValue = document.getElementById("nameText");
-//     let userEmailValue = document.getElementById("emailText");
-//     let userPasswordValue = document.getElementById("passwordText");
-//     let invalidData1 = document.getElementById("invalid-feedback1"); /* name div de invalid */
-//     let invalidData2 = document.getElementById("invalid-feedback2"); /* name div de invalid */
-//     let invalidData3 = document.getElementById("invalid-feedback3"); /* name div de invalid */
-    
-
-//     /* validar si el input no es vacìo, enviar formulario */
-
-//     if((userNameValue.value !=="") && (userEmailValue.value !=="") && (userPasswordValue.value !=="")){
-//         usuario.name = userNameValue.value;
-//         usuario.email = userEmailValue.value;
-//         usuario.password = userPasswordValue.value;
-//         console.log(usuario);
-//         datoInvalido();
-//     }
-
-//     else{
-//         borrarDatosInput();
-//     }
-
-
-//     /* si es invàlido o vacìo el dato */
-     
-
-//     function datoInvalido(){
-
-//         if (userNameValue.value ==""){
-//             userNameValue.classList.add("invalid");
-//             invalidData1.style.display = "block";
-//         }
-        
-//         else if (userEmailValue.value ==""){
-//             userEmailValue.classList.add("invalid");
-//             invalidData2.style.display = "block";
-//         }
-//         else if (userPasswordValue.value ==""){
-//             userPasswordValue.classList.add("invalid");
-//             invalidData3.style.display = "block";
-//         }
-//         else{
-//             console.log("nada")
-//         }
-//         console.log(usuario)
-    
-//     }
-
-    
-//     /* si es vàlido el dato */
-
-//     function datoValido(){
-       
-//     if (userNameValue.value !==""){
-//         userNameValue.classList.remove("invalid");
-//         userNameValue.classList.add("valid");
-//         invalidData1.style.display = "none";
-//     }
-
-//     if (userEmailValue.value !==""){
-//         userEmailValue.classList.remove("invalid");
-//         userEmailValue.classList.add("valid");
-//         invalidData2.style.display = "none";
-//     }
-
-//     if (userPasswordValue.value !==""){
-//         userPasswordValue.classList.remove("invalid");
-//         userPasswordValue.classList.add("valid");
-//         invalidData3.style.display = "none";
-//     }
-//     }
-
-//     function borrarDatosInput(){
-        
-//         userNameValue.value = "";
-//         userEmailValue.value = "";
-//         userPasswordValue.value = "";
-//         userNameValue.classList.remove("valid");
-//         userNameValue.classList.remove("invalid");
-//         userEmailValue.classList.remove("valid");
-//         userEmailValue.classList.remove("invalid");
-//         userPasswordValue.classList.remove("valid");
-//         userPasswordValue.classList.remove("invalid");
-//         invalidData1.style.display = "none";
-//         invalidData2.style.display = "none";
-//         invalidData3.style.display = "none";
-
-//     }
-
-//     datoValido();
-//     datoInvalido();
-//     // enterLogin();
-    
+/* GUARDAR LISTA EN SECTTION STORAGE*/
 
 
 
-    
+// localStorage.getItem('lista usuarios',JSON.parse(LISTA_USUARIO)); /* PARA COGER RICO*/
 
-// })
+let loginBox = document.getElementById("login-section");
+
+function clickLogin(){ 
+
+    if(loginStyleCounter == 1){
+        formBoxLogin.style.top = "85%";
+    }
+}
 
 
-// function enterLogin(){
-//     let formBoxLogin = document.getElementById("login-form-box");
-//     formBoxLogin.style.top = "32%";
-//     console.log(formBoxLogin);
-//     console.log("LOGFORM")
-
-
-
-// }
---
+++ js/userLogged.js
@@ -0,0 +1,19 @@
+let lista_usuarios = sessionStorage.getItem('listaUsuarios')
+
+lista_usuarios = JSON.parse(lista_usuarios);
+
+console.log(lista_usuarios);
+
+
+let usuario = sessionStorage.getItem('usuario')
+
+usuario = JSON.parse(usuario);
+
+console.log(usuario);
+// function genderFunction(){
+    
+//     if(UsergenderSelect.value == "female"){
+
+//     }
+        
+// }
